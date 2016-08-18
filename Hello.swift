d76b8652b8e7aa717fafd324b420cb4be5158e2e
Thomas Franceschini <tumasgiu@mac.com>
2016-08-18 17:04:21 +0200

    initial commit

---
+++ Hello.swift
@@ -0,0 +1,5 @@
+func hello() {
+  print("hello windows!")
+}
+
+hello()
---
+++ HelloW32.swift
@@ -0,0 +1,23 @@
+import CWin32
+
+enum MessageBox {
+    struct Options: OptionSet {
+        var rawValue: UInt32
+
+        static let ok = Options(rawValue: 0)
+        static let abortRetryIgnore = Options(rawValue: 0x2)
+        static let iconExclamation = Options(rawValue: 0x30)
+        static let cancelTryContinue = Options(rawValue: 0x6)
+        static let help = Options(rawValue: 0x4000)
+    }
+}
+
+func displayMessageBox(message: String, title: String = "Error", options: MessageBox.Options = .ok) {
+    MessageBoxA(nil, message, title, options.rawValue)
+}
+
+func hello() {
+    displayMessageBox(message: "Hello windows!", title: "Swift", options: [.iconExclamation, .help])
+}
+
+hello()
